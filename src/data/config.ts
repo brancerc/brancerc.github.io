55aad374cf1cce29859fe20c377d7214261ef6df
Brando Cervantes C. <127698356+brancerc@users.noreply.github.com>
2025-11-21 19:33:52 -0600

    update side info

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,7 +3,7 @@ export const config = {
   description: 'Creo infraestructuras de red robustas y desarrollo aplicaciones que transforman datos en soluciones estratégicas y escalables.',
   contactEmail: 'brandocervantesc@gmail.com',
   keywords: 'portfolio, business intelligence, data analysis, Power BI Reports, Software Engineer, Web Developer',
-  favicon: '/srocket.svg',
+  favicon: '/srocket1.svg',
   social: {
     linkedin: 'https://www.linkedin.com/in/brando-cc/',
     github: 'https://github.com/brancerc'
